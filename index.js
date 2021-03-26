a129be371bda68b94e230d27587b1644276ce6e2
tamanjic <tamanji.che@metropolia.fi>
2021-03-27 00:37:30 +0200

    initial app

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+const app = require('./app');
+const http = require('http');
+const db = require('./db');
+
+const server = http.createServer(app);
+
+db.on('connected', () => {
+  server.listen(process.env.PORT,
+      () => console.log(`App listening on port ${process.env.PORT}!`));
+});
